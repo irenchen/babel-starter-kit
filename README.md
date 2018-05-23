d55b0913e63fdc19d77dc699c089154336aaf1e5
irenchen <iren_chen@hotmail.com>
2018-05-23 13:25:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# babel-tutorial-kit
+
+## Getting started
+
+```bash
+git clone https://github.com/irenchen/babel-starter-kit
+cd babel-starter-kit
+npm install
+npm start
+```
---
+++ app.js
@@ -0,0 +1,7 @@
+import casual from 'casual'
+
+console.log(casual.first_name)
+console.log(casual.last_name)
+console.log(casual.country)
+console.log(casual.latitude)
+console.log(casual.longitude)
